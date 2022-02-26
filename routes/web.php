c5811e1d1a24a775bf321c5c52dc6196110003b2
pnzrkmpfwgn <kurtknispel9@gmail.com>
2022-02-26 18:34:23 +0200

    Liking-unliking posts

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
-
+use App\Http\Controllers\PostController;
 
 Route::get('/',function(){
     return view('home');
@@ -20,6 +20,6 @@ Route::post('logout',[LogoutController::class,'store'])->name('logout');
 
 Route::get('/register',[RegisterController::class,'index'])->name('register');
 Route::post('/register', [RegisterController::class,'store']);
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+
+Route::get('/posts',[PostController::class,'index'])->name('posts');
+Route::post('posts',[PostController::class,'store']);
